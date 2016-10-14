6b150be1ceb373759ef15316d17cfadae48fbe0e
planktonlex55 <planktonlex55@gmail.com>
2016-10-14 09:42:55 +0530

    Create 1_vars.py

---
+++ radical_final/1_basics/1_vars.py
@@ -0,0 +1,23 @@
+num1 = 10
+num2 = 20
+
+num3 = num1 + num2
+print num3 
+print "the total is:", num3
+print "the total is: %d" % num3
+print "the total is: " + str(num3)
+
+float1 = 1.0
+float2 = 3.14
+float3 = float1 + float2
+print "the float total is: %f" % float3
+print "the float total is: %.2f" % float3
+print "the float total is: %0.3f" % float3
+
+str1 = 'hello'
+str2 = 'world'
+str3 = str1 + str2
+print str3
+print "the concatenated string is", str3
+print "the concatenated string is %s" % str3
+
